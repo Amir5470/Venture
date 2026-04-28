1f3193c189158e28268eba3389155568f20bcefd
Amir5470 <amirmechkour5474@gmail.com>
2026-04-27 22:39:03 -0400

    feat: update navigation paths and enhance mobile responsiveness

--- public/script/Login.js
+++ public/script/Login.js
@@ -34,7 +34,7 @@ signupPage.style.display = 'none'
 
 // If already logged in, skip the login page entirely
 onAuthStateChanged(auth, user => {
-    if (user) window.location.href = "./home.html"
+    if (user) window.location.href = "../../home"
 })
 
 modeSwitch.addEventListener('change', () => {
@@ -74,7 +74,7 @@ GoogleBtn.onclick = async () => {
     try {
         const res = await signInWithPopup(auth, googleProvider)
         await saveUserData(res.user)
-        window.location.href = "../home"
+        window.location.href = "../../home"
     } catch (e) { handleError(e) }
 }
 
@@ -82,7 +82,7 @@ GithubBtn.onclick = async () => {
     try {
         const res = await signInWithPopup(auth, githubProvider)
         await saveUserData(res.user)
-        window.location.href = "../home"
+        window.location.href = "../../home"
     } catch (e) { handleError(e) }
 }
 
@@ -92,7 +92,7 @@ submitform.onclick = async () => {
     try {
         const res = await signInWithEmailAndPassword(auth, emailVal, passVal)
         await saveUserData(res.user)
-        window.location.href = "../home"
+        window.location.href = "../../home"
     } catch (e) { handleError(e) }
 }
 
@@ -104,7 +104,7 @@ registerBtn.onclick = async () => {
         const userCred = await createUserWithEmailAndPassword(auth, emailVal, passVal)
         await updateProfile(userCred.user, { displayName: username })
         await saveUserData(userCred.user)
-        window.location.href = "../home"
+        window.location.href = "../../home"
     } catch (e) { handleError(e) }
 }
 
